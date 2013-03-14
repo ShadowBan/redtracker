9c51ddd4f3d4ebd08733b464b3bc37986ab9fa4c
Chris Kincanon <Catsith@gmail.com>
2013-03-14 13:06:42 -0700

    using right pub date

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.sort("published_date desc").all
+    @posts = Post.sort("published_at desc").all
 
     respond_to do |format|
       format.html # index.html.erb
