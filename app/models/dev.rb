5a8969215fc3a808d829085a7617e3ac6bea08e5
Chris Kincanon <Catsith@gmail.com>
2013-03-18 23:01:26 -0700

    grabbing the devs name from the page

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -2,10 +2,9 @@ class Dev < ActiveRecord::Base
   attr_accessible :name, :username
   has_many :posts, :dependent => :destroy
 
-  def self.find_or_create username
-    return nil if username.blank?
-    dev = Dev.where(:username=>username).first
-    dev = Dev.create(:username=>username,:name=>username.gsub(/\..*/i,"")) unless dev
+  def self.find_or_create name
+    dev = Dev.where(:name=>name).first
+    dev = Dev.create(:name=>name) unless dev
     dev
   end
 end
