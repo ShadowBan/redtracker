cf64c4f347eb6779547d333de5c931c6f378db5d
Chris Kincanon <Catsith@gmail.com>
2013-03-31 02:12:13 -0700

    ajaxifying the categories

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -8,5 +8,25 @@ $(".category").change(function() {
         $(this).find('~ ul > li > label').addClass("off");
         $(this).find('~ ul > li > input').removeAttr("checked");  
       }
+    }else{
+      if(this.checked) {
+        $(this).parent().parent().parent().find('> label').removeClass("off");
+        $(this).parent().parent().parent().find('> input').prop("checked", true);
+      }
     }
+    updateStream($(this).closest("form"))
 });
+
+
+function updateStream($form){
+  $.ajax({
+    url: $form.attr("action"),
+    data: $form.serialize(),
+    type: 'GET',
+    dataType: 'script',
+    complete: function(data,status,jqXHR) {
+      $('#stream').html(data.responseText);
+    }
+  });
+  return false;
+}
